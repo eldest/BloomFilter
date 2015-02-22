1adbef4ece07519b480618277a27d9367d478559
eldest <eldest.007@gmail.com>
2015-02-22 22:28:16 +0500

    update

--- src/test/java/com/eldest/bloomfilter/hash/HashTest.java
+++ src/test/java/com/eldest/bloomfilter/hash/HashTest.java
@@ -22,7 +22,7 @@ public class HashTest {
 
     //--------------------------------- support ---------------------------------
 
-    private static void testHash(final String[] input, final HashFunction hashFunction) {
+    private static void testHash(final HashFunction hashFunction) {
         for (String item : Arrays.asList(INPUT)) {
             long hash = hashFunction.hash(item);
             int index = hashFunction.index(item, FILTER_SIZE);
@@ -41,71 +41,71 @@ public class HashTest {
 
     @Test
     public void testMurmur() throws Exception {
-        testHash(INPUT, Hashes.MURMUR);
+        testHash(Hashes.MURMUR);
     }
 
     @Test
     public void testFNV1a32() throws Exception {
-        testHash(INPUT, Hashes.FNV1A32);
+        testHash(Hashes.FNV1A32);
     }
 
     @Test
     public void testFNV1a64() throws Exception {
-        testHash(INPUT, Hashes.FNV1A64);
+        testHash(Hashes.FNV1A64);
     }
 
     @Test
     public void testFNV132() throws Exception {
-        testHash(INPUT, Hashes.FNV_132);
+        testHash(Hashes.FNV_132);
     }
 
     @Test
     public void testFNV164() throws Exception {
-        testHash(INPUT, Hashes.FNV_164);
+        testHash(Hashes.FNV_164);
     }
 
     @Test
     public void testMD5() throws Exception {
-        testHash(INPUT, Hashes.MD5);
+        testHash(Hashes.MD5);
     }
 
     @Test
     public void testSHA_1() throws Exception {
-        testHash(INPUT, Hashes.SHA_1);
+        testHash(Hashes.SHA_1);
     }
 
     @Test
     public void testSHA_256() throws Exception {
-        testHash(INPUT, Hashes.SHA_256);
+        testHash(Hashes.SHA_256);
     }
 
     @Test
     public void testGoogleMURMUR3_32() throws Exception {
-        testHash(INPUT, Hashes.Google.MURMUR3_32);
+        testHash(Hashes.Google.MURMUR3_32);
     }
 
     @Test
     public void testGoogleADLER32() throws Exception {
-        testHash(INPUT, Hashes.Google.ADLER32);
+        testHash(Hashes.Google.ADLER32);
     }
 
     @Test
     public void testGoogleCRC32() throws Exception {
-        testHash(INPUT, Hashes.Google.CRC32);
+        testHash(Hashes.Google.CRC32);
     }
 
     @Test
     public void testGoogleMD5() throws Exception {
-        testHash(INPUT, Hashes.Google.MD5);
+        testHash(Hashes.Google.MD5);
     }
 
     @Test
     public void testGoogleSHA256() throws Exception {
-        testHash(INPUT, Hashes.Google.SHA256);
+        testHash(Hashes.Google.SHA256);
     }
 
     @Test
     public void testGoogleSIPHASH24() throws Exception {
-        testHash(INPUT, Hashes.Google.SIPHASH24);
+        testHash(Hashes.Google.SIPHASH24);
     }
 }
